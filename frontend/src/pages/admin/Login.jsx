88a85dfbb08becf3b0628d1a0e6930925a24a87c
Khushal saboo <khushalsaboo108@gmail.com>
2024-03-05 11:14:50 +0530

    start

--- frontend/src/pages/admin/Login.jsx
+++ frontend/src/pages/admin/Login.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 export function Login() {
   return (
-    <div>Login</div>
+    <div className=' container mx-auto'>
+      <div className=' row justify-content-center align-items-center '>
+        <div className=" p-1 my-4 " >dd</div>
+      </div>
+    </div>
   )
 }
