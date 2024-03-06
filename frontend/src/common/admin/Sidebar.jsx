3b0a6952615c17cc6c66f0fba6563f4d8e57b14c
Khushal saboo <khushalsaboo108@gmail.com>
2024-03-06 10:24:40 +0530

    dashboard_header&sidebar

--- frontend/src/common/admin/Sidebar.jsx
+++ frontend/src/common/admin/Sidebar.jsx
@@ -2,6 +2,7 @@ import React, { useContext } from 'react'
 import { adminContext } from '../../Contextdata/AdminContext'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUser } from '@fortawesome/free-solid-svg-icons'
+import { Link } from 'react-router-dom'
 
 export default function Sidebar() {
   let { sidBar, setSitBar } = useContext(adminContext)
@@ -10,9 +11,34 @@ export default function Sidebar() {
       <div className={(sidBar) ? 'w-[218px] shadow-md' : 'w-[137px] shadow-md'}>
         <div className=' p-[10px] w-[100%] '>
           <ul>
-            <h2 className='text-center font-semibold text-[25px]'>Welcome</h2>
-            <li className=''>
+            <h2 className='text-center font-semibold text-[30px]'>Welcome</h2>
+            <li className='mt-[20px]'>
+              <Link to={'/admin/dashboard'} className=' text-[25px]  m-[0] font-semibold'>
+                <FontAwesomeIcon icon={faUser} className=' ps-5 pe-[8px] text-[20px]' />
+                Dashboard
+              </Link>
+            </li>
+            <li className='mt-[10px]'>
+              <Link to={'/admin/dashboard'} className=' text-[25px]  m-[0] font-semibold'>
+                <FontAwesomeIcon icon={faUser} className=' ps-5 pe-[8px] text-[20px]e' />
+                Catagery
+              </Link>
+            </li>
+            <li className='mt-[10px]'>
               <FontAwesomeIcon icon={faUser} className=' ps-5 text-[20px]' />
+              <Link to={'/admin/dashboard'} className=' text-[25px] ps-[8px] m-[0] font-semibold'>product</Link>
+            </li>
+            <li className='mt-[10px]'>
+              <FontAwesomeIcon icon={faUser} className=' ps-5 text-[20px]' />
+              <Link to={'/admin/dashboard'} className=' text-[25px] ps-[8px] m-[0] font-semibold'>colour</Link>
+            </li>
+            <li className='mt-[10px]'>
+              <FontAwesomeIcon icon={faUser} className=' ps-5 text-[20px]' />
+              <Link to={'/admin/dashboard'} className=' text-[25px] ps-[8px] m-[0] font-semibold'>user</Link>
+            </li>
+            <li className='mt-[10px]'>
+              <FontAwesomeIcon icon={faUser} className=' ps-5 text-[20px]' />
+              <Link to={'/admin/dashboard'} className=' text-[25px] ps-[8px] m-[0] font-semibold'>order</Link>
             </li>
           </ul>
         </div>
